dddf5becce38e158f86c472cafbd26e2086b9cec
Jkim-21 <113380851+Jkim-21@users.noreply.github.com>
2023-09-01 09:29:21 -0400

    Complete Exercise 5
    We practice assigning manipulating arguments based on different
    situations and taking account for lots of edge cases.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(firstInteger, secondInteger) {
+    if ((firstInteger < 0) || (secondInteger < 0) || !Number.isInteger(firstInteger) || !Number.isInteger(secondInteger)) {
+        return "ERROR";
+    }
+    let sum = 0;
+    let smallerInteger = Math.min(firstInteger, secondInteger);
+    let biggerInteger = Math.max(firstInteger, secondInteger);
+    for(let i = smallerInteger; i <= biggerInteger; i++) {
+        sum += i;
+    }
+    return sum;
+};
 
+const sumAll2 = function(min, max) {
+    if ((min < 0) || (max < 0) || !Number.isInteger(min) || !Number.isInteger(max)) {
+        return "ERROR";
+    }
+    let sum = 0;
+    if (min > max) {
+        const temp = min;
+        min = max;
+        max = temp;
+        }
+
+    for(let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
