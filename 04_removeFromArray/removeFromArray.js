0d5eae9d8e66a799ef39dbd3bcb8749d7d5bdba8
Jkim-21 <113380851+Jkim-21@users.noreply.github.com>
2023-09-01 09:07:06 -0400

    Complete Exercise 4
    The purpose of this exercise is to manipulate arrays and exploring
    rest parameters.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(numberArray, ...removedNumbers) {
+    for (const removingNumber of removedNumbers) {
+        let removingNumberIndex = numberArray.indexOf(removingNumber);
+        if (removingNumberIndex !== -1) {
+            numberArray.splice(removingNumberIndex, 1);
+        }
+    }
+    return numberArray;
 };
 
 // Do not edit below this line
