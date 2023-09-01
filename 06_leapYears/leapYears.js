a8d6ce18194e6590f55a8dc41726536fa988bd92
Jkim-21 <113380851+Jkim-21@users.noreply.github.com>
2023-09-01 09:41:24 -0400

    Complete Exercise 6
    This exercise works on using && and % operators as well as finding
    the correct boolean value depending on many nesting conditions.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,31 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return true;
+        }
+    }
+    else {
+        return false;
+    }
 };
+
+const leapYears2 = function(year) {
+    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 
 // Do not edit below this line
 module.exports = leapYears;
