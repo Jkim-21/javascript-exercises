0ac6f01414f09a71d8c57e9fa6b0500441771a60
Jkim-21 <113380851+Jkim-21@users.noreply.github.com>
2023-09-01 09:55:20 -0400

    Complete Exercise 7
    The purpose of the exercise is to explore how to place the calculated
    value to one decimal as well as implementing the correct order of
    operations.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitNumber) {
+  let celsiusNumber = ((fahrenheitNumber - 32) * (5/9));
+  return Math.round(celsiusNumber * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusNumber) {
+  let fahrenheitNumber = (celsiusNumber * (9/5) + 32);
+  return Math.round(fahrenheitNumber * 10) / 10
 };
 
 // Do not edit below this line
